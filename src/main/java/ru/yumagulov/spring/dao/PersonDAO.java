812105754ee7d43178d76c0dfcb5761807c05653
Il'dar <yumagulov.ildar@gmail.com>
2022-03-20 20:45:28 +0500

    CRUD DAO

---
+++ src/main/java/ru/yumagulov/spring/dao/PersonDAO.java
@@ -0,0 +1,38 @@
+package ru.yumagulov.spring.dao;
+
+import org.springframework.stereotype.Component;
+import ru.yumagulov.spring.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        Person person = null;
+        for (int i = 0; i < people.size(); i++)
+            if(id == people.get(i).getId())
+            {
+                person = people.get(i);
+                break;
+            }
+
+        return person;
+    }
+}
